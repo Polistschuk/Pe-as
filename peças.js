5ed6ff364a8689465c3c414914adf0ee2962e840
Julio Polistschuk <juliopolistschukw@gmail.com>
2021-12-11 18:58:59 -0300

    ta pronto

---
+++ peças.js
@@ -0,0 +1,14 @@
+var listaDePecas = ["Amortecerdor","Motor","Filtro do Ar"];
+
+let peso = 100;
+let nomePeca = "OI";
+
+if(nomePeca.length < 3 && peso >= 100 && listaDePecas.length < 4){
+    console.log("Otimo nome : " +nomePeca + " Adorei, O peso - " +peso+ " - esta adequado, peça cadastrada")
+}else{
+    console.log("Verifique se o peso é no minimo 100g, se o nome da peça não excede 3 letras e se a lista possui menos de 4 itens")
+}
+
+
+       
+
